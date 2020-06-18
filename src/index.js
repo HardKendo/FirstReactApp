faf5ae55f2bf688e5d9c96fc48054bad6796e9b3
HardKendo <yaroslaw.priga@yandex.ru>
2020-06-18 10:19:21 +0300

    Output data from all components to a singl file.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,33 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let dialogsData = 
+[
+    { id: 1, name: 'Dima' },
+    { id: 2, name: 'Andrey' },
+    { id: 3, name: 'Timur' },
+    { id: 4, name: 'Ivan' },
+    { id: 5, name: 'Artem' }
+]
+
+let messagesData = 
+[
+    { id: 1, message: 'Hi' },
+    { id: 2, message: 'How are you?' },
+    { id: 3, message: 'Yo' }
+]
+
+let postData = 
+[
+    { id: 1, post: 'Hi, how are you?', like: '15' },
+    { id: 1, post: 'It\'s my first post', like: '20' }
+]
+
+
+
+ReactDOM.render(<App postData={postData} dialogsData={dialogsData} messagesData={messagesData}/>, document.getElementById('root'));
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
