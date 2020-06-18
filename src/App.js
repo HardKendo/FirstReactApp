0e1bc9ce424069f32b304b2d7a1f6a97e8c1eeb2
HardKendo <yaroslaw.priga@yandex.ru>
2020-06-18 10:17:52 +0300

    Add teg Route.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import React from 'react';
+import { Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from './components/Dialogs/Dialogs';
+import Music from './components/Music/Music';
+import News from './components/News/News';
+import Settings from './components/Settings/Settings';
 
 
- 
+
 const App = (props) => {
   return (
-    <div className='app-wrapper'>
+    <BrowserRouter>
       <Header />
-      <Navbar />
-      <div className='app-wrapper-content'>
-       <Dialogs />
+      <div className='app-wrapper'>
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs' render={ () => <Dialogs dialogsData={props.dialogsData} messagesData={props.messagesData}/>} />
+          <Route path='/profile' render={ () => <Profile postData={props.postData} /> }/>
+          <Route path='/news' render={ () => <News />} />
+          <Route path='/music' render={ () => <Music />} />
+          <Route path='/settings' render={ () => <Settings />} />
+        </div>
       </div>
-      {/* <Profile /> */}
-    </div>
+    </BrowserRouter>
+
   );
 }
 
